377e2a09176caef9735274300018b69fb9247e5b
AkramALJabari <64495122+AkramALJabari@users.noreply.github.com>
2022-05-29 01:57:02 -0700

    Add files via upload

---
+++ commands/poker.js
@@ -0,0 +1,31 @@
+//755827207812677713
+const fetch = require("node-fetch");
+module.exports = {
+  name: "poker",
+  aliases: [],
+  description: "تشغيل ميزة بوكر",
+  async execute(message, args) {
+    let channel = message.member.voice.channel;
+    if(!channel) return message.channel.send("يجب عليك دخول روم صوتي اولاً")
+    await fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+          method: 'POST',
+          body: JSON.stringify({
+            max_age: 86400,
+            max_uses: 0,
+            target_application_id: "755827207812677713",
+            target_type: 2,
+            temporary: false,
+            validate: null,
+          }),
+          headers: {
+            Authorization: `Bot ${message.client.token}`,
+            'Content-Type': 'application/json',
+          },
+        })
+    .then((res) => res.json())
+    .then(invite => {
+        if(!invite.code) return message.channel.send(`**I can't start the activity**`)
+        message.channel.send(`https://discord.com/invite/${invite.code}`)
+    })
+  }
+};
